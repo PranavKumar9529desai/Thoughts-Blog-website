55b5b1dc3e89b37680e3bffb8976de662d0bece4
PranavKumar9529desai <dpranav7745@gmail.com>
2024-05-28 21:38:07 +0530

    done with the ui

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -7,7 +7,7 @@ function signUp() {
   const blogs = useRecoilValue(blogsState);
   console.log( "blogs are : ", blogs.length) //
   return (
-      <div className='lg:grid grid-cols-2 flex justify-center items-center h-screen bg-slate-200'>
+      <div className='lg:grid grid-cols-2 flex justify-center items-center h-screen bg-slate-200 sm:bg-slate-50'>
           <div className='flex justify-center items-center'><Auth type='signup'/></div>
           <div className='hidden lg:block'><Quote /></div>
       </div> 
