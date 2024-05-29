5c6e76fe1af0e5aa11fa3e375f1eee3d517f351b
PranavKumar9529desai <dpranav7745@gmail.com>
2024-05-29 13:07:21 +0530

    done with this

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,7 +4,7 @@ import medium from "@assets/medium.png";
 
 export function Appbar() {
   return (
-    <div className="z-40 bg-slate-200 lg:w-full w-screen flex justify-between items-center h-[4rem] border-b border-slate-600 fixed ">
+    <nav className="z-50 bg-slate-200 lg:w-full w-screen flex justify-between items-center h-[4rem] border-b border-slate-600 fixed top-0 ">
       <div className="text-3xl font-extrabold lg:ml-10 ml-5 font-montserrat flex">
         <img
           alt="medium logo"
@@ -16,6 +16,6 @@ export function Appbar() {
       <div className="lg:mr-20 mr-12 shadow-md shadow-slate-800 rounded-full   hover:shadow-2xl hover:-translate-y-1  transition-all duration-300 ">
         <AvatarDropDown size="small" />
       </div>
-    </div>
+    </nav>
   );
 }
--- frontend/src/routes/blogs.tsx
+++ frontend/src/routes/blogs.tsx
@@ -16,11 +16,13 @@ export function Blogs() {
 
   return (
     <>
-      <Appbar />
-      <div className="flex  items-center flex-col max">
+      <div>
+        <Appbar />
+      </div>
+      <div className="mt-10 flex  items-center flex-col max">
         {Loading ? (
           <>
-            <div className="lg:mt-20 lg:flex  flex-col items-center w-full lg:px-4 px-12 pt-10 mt-20">
+            <div className="lg:mt-1 lg:flex  flex-col items-center w-full lg:px-4 px-12 pt-10 mt-20">
               <CustomSkelton blogCount={3} />
             </div>
           </>
@@ -28,51 +30,50 @@ export function Blogs() {
           // <div className="lg:mt-20 lg:flex  flex-col items-center w-full px-4 mt-20">
           //   <CustomSkelton blogCount={3} />
           // </div>
-            <div className="mt-20">
-              {Blogs.map((blog, index) => {
-                const imageUrl = extractImage(blog.content);
-                const contentWithoutImage = blog.content.replace(/<img.*?>/g, "");
-                return (
-                  <div
-                    key={index}
-                    className="lg:w-[900px] py-1 border-b border-slate-400"
-                  >
-                    {/* TODO make the publishedDate dynamic  */}
-                    <div className="flex justify-between rounded-xl">
-
+          <div className="mt-6">
+            {Blogs.map((blog, index) => {
+              const imageUrl = extractImage(blog.content);
+              const contentWithoutImage = blog.content.replace(/<img.*?>/g, "");
+              return (
+                <div
+                  key={index}
+                  className="lg:w-[900px] py-1 border-b border-slate-400"
+                >
+                  {/* TODO make the publishedDate dynamic  */}
+                  <div className="flex justify-between rounded-xl">
                     <div className="grid grid-cols-12 ">
-                        <div className="col-span-8 ">
-                          <Link to={"/blogs/" + blog.id}>
-                            <BlogsCard
-                              AuthorName={
-                                blog.author.name ? blog.author.name : "Anonymous"
-                              }
-                              // date with specific format
-                              publishedDate={new Date(
-                                blog.createdAt
-                              ).toLocaleDateString("en-GB", {
-                                day: "2-digit",
-                                month: "short",
-                                year: "numeric",
-                              })}
-                              title={blog.title}
-                              // passing down the text without image
-                              description={contentWithoutImage}
-                            />
-                          </Link>
-                        </div>
-                        <div className="hidden lg:flex col-span-4 w-full h-full  justify-center items-center rounded-xl ">
-                          <img
-                            src={imageUrl}
-                            className=" w-[170px] h-[140px] rounded-lg shadow-lg shadow-slate-600 mx-auto mt-2"
+                      <div className="col-span-8 ">
+                        <Link to={"/blogs/" + blog.id}>
+                          <BlogsCard
+                            AuthorName={
+                              blog.author.name ? blog.author.name : "Anonymous"
+                            }
+                            // date with specific format
+                            publishedDate={new Date(
+                              blog.createdAt
+                            ).toLocaleDateString("en-GB", {
+                              day: "2-digit",
+                              month: "short",
+                              year: "numeric",
+                            })}
+                            title={blog.title}
+                            // passing down the text without image
+                            description={contentWithoutImage}
                           />
-                        </div>
+                        </Link>
+                      </div>
+                      <div className="hidden lg:flex col-span-4 w-full h-full  justify-center items-center rounded-xl ">
+                        <img
+                          src={imageUrl}
+                          className=" w-[170px] h-[140px] rounded-lg shadow-lg shadow-slate-600 mx-auto mt-2"
+                        />
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
+                </div>
+              );
+            })}
+          </div>
         )}
       </div>
       <div className=" top-[520px] right-[90px] lg:top-[600px] lg:right-[100px] rounded-full fixed ">
