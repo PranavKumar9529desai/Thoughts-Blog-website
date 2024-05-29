f99f0e0e719c7dfc36f565d5040f503662f006bf
PranavKumar9529desai <dpranav7745@gmail.com>
2024-05-29 10:12:08 +0530

    optimized the responsiveness for phone view

--- frontend/src/routes/blogs.tsx
+++ frontend/src/routes/blogs.tsx
@@ -25,51 +25,54 @@ export function Blogs() {
             </div>
           </>
         ) : (
-          <div className="mt-20">
-            {Blogs.map((blog, index) => {
-              const imageUrl = extractImage(blog.content);
-              const contentWithoutImage = blog.content.replace(/<img.*?>/g, "");
-              return (
-                <div
-                  key={index}
-                  className="lg:w-[900px] py-1 border-b border-slate-400"
-                >
-                  {/* TODO make the publishedDate dynamic  */}
-                  <div className="flex justify-between rounded-xl">
+          // <div className="lg:mt-20 lg:flex  flex-col items-center w-full px-4 mt-20">
+          //   <CustomSkelton blogCount={3} />
+          // </div>
+            <div className="mt-20">
+              {Blogs.map((blog, index) => {
+                const imageUrl = extractImage(blog.content);
+                const contentWithoutImage = blog.content.replace(/<img.*?>/g, "");
+                return (
+                  <div
+                    key={index}
+                    className="lg:w-[900px] py-1 border-b border-slate-400"
+                  >
+                    {/* TODO make the publishedDate dynamic  */}
+                    <div className="flex justify-between rounded-xl">
 
-                  <div className="grid grid-cols-12 ">
-                      <div className="col-span-8 w-[600px]">
-                        <Link to={"/blogs/" + blog.id}>
-                          <BlogsCard
-                            AuthorName={
-                              blog.author.name ? blog.author.name : "Anonymous"
-                            }
-                            // date with specific format
-                            publishedDate={new Date(
-                              blog.createdAt
-                            ).toLocaleDateString("en-GB", {
-                              day: "2-digit",
-                              month: "short",
-                              year: "numeric",
-                            })}
-                            title={blog.title}
-                            // passing down the text without image
-                            description={contentWithoutImage}
+                    <div className="grid grid-cols-12 ">
+                        <div className="col-span-8 ">
+                          <Link to={"/blogs/" + blog.id}>
+                            <BlogsCard
+                              AuthorName={
+                                blog.author.name ? blog.author.name : "Anonymous"
+                              }
+                              // date with specific format
+                              publishedDate={new Date(
+                                blog.createdAt
+                              ).toLocaleDateString("en-GB", {
+                                day: "2-digit",
+                                month: "short",
+                                year: "numeric",
+                              })}
+                              title={blog.title}
+                              // passing down the text without image
+                              description={contentWithoutImage}
+                            />
+                          </Link>
+                        </div>
+                        <div className="hidden lg:flex col-span-4 w-full h-full  justify-center items-center rounded-xl ">
+                          <img
+                            src={imageUrl}
+                            className=" w-[170px] h-[140px] rounded-lg shadow-lg shadow-slate-600 mx-auto mt-2"
                           />
-                        </Link>
-                      </div>
-                      <div className="hidden lg:flex col-span-4 w-full h-full  justify-center items-center rounded-xl ">
-                        <img
-                          src={imageUrl}
-                          className=" w-[170px] h-[140px] rounded-lg shadow-lg shadow-slate-600 mx-auto mt-2"
-                        />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
         )}
       </div>
       <div className=" top-[520px] right-[90px] lg:top-[600px] lg:right-[100px] rounded-full fixed ">
